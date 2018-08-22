7d603128656f20ec31104499982602c1883168b9
ajz003 <ajz003@ucla.edu>
2018-08-22 16:50:03 -0700

    Add Basic HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var guessesLeft = 6
 
 var dictionary = ["scala", "swift", "shell", "typescript", "c", "go", "css", "php", "ruby", "java", "python", "javascript"];
 
-var computerGuess = dictionary[Math.floor(Math.random()*dictionary.length)]
+var computerGuess = dictionary[Math.floor(Math.random() * dictionary.length)]
 console.log("comp guess: " + computerGuess)
 
 var wordDisplay = [];
@@ -16,58 +16,66 @@ for (var i = 0; i < computerGuess.length; i++) {
 console.log(wordDisplay)
 
 for (var i = 0; i < computerGuess.length; i++) {
-    _Display.push("_");
+    _Display.push("_ ");
 }
 console.log(_Display)
 
-
 var prevGuesses = [];
 
-
-
+console.log("wins :" + wins)
 
 
 
 
 document.onkeyup = function (event) {
 
-userGuess = event.key
-console.log("userGuess: " + userGuess)
+    userGuess = event.key
+    console.log("userGuess: " + userGuess)
 
 
-for (var i = 0; i < wordDisplay.length; i++) {
+    for (var i = 0; i < wordDisplay.length; i++) {
 
-    if (wordDisplay[i] === event.key) {
-        _Display.splice(i, 1, userGuess);
+        if (wordDisplay[i] === event.key) {
+            _Display.splice(i, 1, userGuess);
+        }
+
+
     }
 
-    
-}
-
-console.log("Previous Guesses: " + prevGuesses);
-console.log("Guesses left: " + guessesLeft);
-
-if (wordDisplay.includes(userGuess) == false && prevGuesses.includes(userGuess) == false) {
-    guessesLeft--;
-    prevGuesses.push(userGuess);
     console.log("Previous Guesses: " + prevGuesses);
     console.log("Guesses left: " + guessesLeft);
-}
 
-if (_Display.toString() == wordDisplay.toString()){
-    alert("You win!");
-    reset();
-}
-console.log(_Display.toString());
-console.log(wordDisplay.toString());
+    if (wordDisplay.includes(userGuess) == false && prevGuesses.includes(userGuess) == false) {
+        guessesLeft--;
+        prevGuesses.push(userGuess);
+        console.log("Previous Guesses: " + prevGuesses);
+        console.log("Guesses left: " + guessesLeft);
+    }
 
-if (guessesLeft === 0) {
-    alert("You lose.");
-    reset();
-}
+    if (_Display.toString() == wordDisplay.toString()) {
+        alert("You win! The word was " + wordDisplay.join('').toUpperCase() + " !");
+        reset();
+    }
+    console.log(_Display.toString());
+    console.log(wordDisplay.toString());
+
+    if (guessesLeft === 0) {
+        alert("You lose. The word was " + wordDisplay.join('').toUpperCase());
+        reset();
+    }
 
 
-
+    var w = document.getElementById("wins");
+    w.textContent = wins;
+    
+    var cw = document.getElementById("current-word")
+    cw.textContent = _Display.join('').toUpperCase()
+    
+    var gr = document.getElementById("guesses-remaining")
+    gr.textContent = guessesLeft;
+    
+    var ag = document.getElementById("already-guessed")
+    ag.textContent = prevGuesses
 
 };
 
@@ -75,7 +83,7 @@ function reset() {
 
     guessesLeft = 6;
 
-    computerGuess = dictionary[Math.floor(Math.random()*dictionary.length)]
+    computerGuess = dictionary[Math.floor(Math.random() * dictionary.length)]
     console.log("comp guess: " + computerGuess)
 
     wordDisplay = [];
@@ -83,12 +91,12 @@ function reset() {
     _Display = [];
 
     for (var i = 0; i < computerGuess.length; i++) {
-    wordDisplay.push(computerGuess.charAt(i));
+        wordDisplay.push(computerGuess.charAt(i));
     }
     console.log(wordDisplay)
 
     for (var i = 0; i < computerGuess.length; i++) {
-    _Display.push("_");
+        _Display.push("_ ");
     }
     console.log(_Display)
 

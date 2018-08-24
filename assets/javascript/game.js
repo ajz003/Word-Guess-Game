340e0b645685f21840a6b805cca930f38ec624e2
ajz003 <ajz003@ucla.edu>
2018-08-23 19:51:38 -0700

    Complete first draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,89 +1,31 @@
 window.onload = function () {
 
-var wins = 0
-var guessesLeft = 8
+    var wins = 0
+    var guessesLeft = 8
 
-var dictionary = ["scala", "swift", "shell", "typescript", "c", "go", "css", "php", "ruby", "java", "python", "javascript"];
+    var dictionary = ["scala", "swift", "shell", "typescript", "c", "go", "css", "php", "ruby", "java", "python", "javascript"];
 
-var oldWords = [];
-var computerGuess = dictionary[Math.floor(Math.random() * dictionary.length)]
-oldWords.push(computerGuess);
-console.log(oldWords)
+    var oldWords = [];
+    var computerGuess = dictionary[Math.floor(Math.random() * dictionary.length)]
 
-
-
-console.log("comp guess: " + computerGuess)
-
-var wordDisplay = [];
-var _Display = [];
-
-for (var i = 0; i < computerGuess.length; i++) {
-    wordDisplay.push(computerGuess.charAt(i));
-}
-
-for (var i = 0; i < computerGuess.length; i++) {
-    _Display.push("_ ");
-}
-
-var prevGuesses = [];
-
-var w = document.getElementById("wins");
-w.textContent = wins;
-
-var cw = document.getElementById("current-word")
-cw.textContent = _Display.join('').toUpperCase()
-
-var gr = document.getElementById("guesses-remaining")
-gr.textContent = guessesLeft;
-
-var ag = document.getElementById("already-guessed")
-ag.textContent = prevGuesses
+    console.log(oldWords)
 
 
 
+    console.log("comp guess: " + computerGuess)
 
-document.onkeyup = function (event) {
+    var wordDisplay = [];
+    var _Display = [];
 
-if (oldWords.length != dictionary.length) {
-
-        if (event.keyCode >= 65 && event.keyCode <= 90) {
-            userGuess = event.key
-        }
-    console.log("userGuess: " + userGuess)
-
-
-    for (var i = 0; i < wordDisplay.length; i++) {
-
-        if (wordDisplay[i] === event.key) {
-            _Display.splice(i, 1, userGuess);
-        }
-
-
+    for (var i = 0; i < computerGuess.length; i++) {
+        wordDisplay.push(computerGuess.charAt(i));
     }
 
-    console.log("Previous Guesses: " + prevGuesses);
-    console.log("Guesses left: " + guessesLeft);
-
-    if (wordDisplay.includes(userGuess) == false && prevGuesses.includes(userGuess) == false) {
-        guessesLeft--;
-        prevGuesses.push(userGuess);
-        console.log("Previous Guesses: " + prevGuesses);
-        console.log("Guesses left: " + guessesLeft);
+    for (var i = 0; i < computerGuess.length; i++) {
+        _Display.push("_ ");
     }
 
-    if (_Display.toString() == wordDisplay.toString()) {
-        alert("You win! The word was " + wordDisplay.join('').toUpperCase() + " !");
-        wins++;
-        reset();
-    }
-    console.log(_Display.toString());
-    console.log(wordDisplay.toString());
-
-    if (guessesLeft === 0) {
-        alert("You lose. The word was " + wordDisplay.join('').toUpperCase());
-        reset();
-    }
-
+    var prevGuesses = [];
 
     var w = document.getElementById("wins");
     w.textContent = wins;
@@ -96,43 +38,108 @@ if (oldWords.length != dictionary.length) {
 
     var ag = document.getElementById("already-guessed")
     ag.textContent = prevGuesses
-}
 
-else {
-    alert("You've guessed all the words! Refresh to restart!");
-}
-};
 
-function reset() {
+
+
+    document.onkeyup = function (event) {
+
+
+
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            userGuess = event.key
+        }
+
+        console.log("userGuess: " + userGuess)
+
+
+        for (var i = 0; i < wordDisplay.length; i++) {
+
+            if (wordDisplay[i] === event.key) {
+                _Display.splice(i, 1, userGuess);
+            }
+
+
+        }
+
+        console.log("Previous Guesses: " + prevGuesses);
+        console.log("Guesses left: " + guessesLeft);
+
+        if (oldWords.length < dictionary.length) {
+            if (wordDisplay.includes(userGuess) == false && prevGuesses.includes(userGuess) == false) {
+                guessesLeft--;
+                prevGuesses.push(userGuess);
+                console.log("Previous Guesses: " + prevGuesses);
+                console.log("Guesses left: " + guessesLeft);
+            }
+        }
+
+        if (_Display.toString() == wordDisplay.toString() && oldWords.length < dictionary.length) {
+            alert("You win! The word was " + wordDisplay.join('').toUpperCase() + " !");
+            wins++;
+            reset();
+        }
+        console.log(_Display.toString());
+        console.log(wordDisplay.toString());
+
+        if (guessesLeft === 0) {
+            alert("You lose. The word was " + wordDisplay.join('').toUpperCase());
+            reset();
+        }
+
+        else if (oldWords.length === dictionary.length) {
+            alert("Game over. Refresh to play again!");
+        }
+
+
+        var w = document.getElementById("wins");
+        w.textContent = wins;
+
+        var cw = document.getElementById("current-word")
+        cw.textContent = _Display.join('').toUpperCase()
+
+        var gr = document.getElementById("guesses-remaining")
+        gr.textContent = guessesLeft;
+
+        var ag = document.getElementById("already-guessed")
+        ag.textContent = prevGuesses
+
+
+
+    };
+
+    function reset() {
 
         guessesLeft = 8;
+        oldWords.push(computerGuess);
+        console.log("oldWordslength: " + oldWords.length)
 
         do {
             computerGuess = dictionary[Math.floor(Math.random() * dictionary.length)]
         }
-        while (oldWords.indexOf(computerGuess) !== -1);
+        while (oldWords.includes(computerGuess) === true && oldWords.length < dictionary.length);
 
-        oldWords.push(computerGuess);
-        
-        console.log("comp guess: " + computerGuess)
 
-        wordDisplay = [];
+        if (oldWords.length < dictionary.length) {
+            console.log("comp guess: " + computerGuess)
 
-        _Display = [];
+            wordDisplay = [];
 
-        for (var i = 0; i < computerGuess.length; i++) {
-            wordDisplay.push(computerGuess.charAt(i));
+            _Display = [];
+
+            for (var i = 0; i < computerGuess.length; i++) {
+                wordDisplay.push(computerGuess.charAt(i));
+            }
+            console.log(wordDisplay)
+
+            for (var i = 0; i < computerGuess.length; i++) {
+                _Display.push("_ ");
+            }
+            console.log(_Display)
+
+
+            prevGuesses = [];
         }
-        console.log(wordDisplay)
-
-        for (var i = 0; i < computerGuess.length; i++) {
-            _Display.push("_ ");
-        }
-        console.log(_Display)
-
-
-        prevGuesses = [];
     }
-    
 }
 
